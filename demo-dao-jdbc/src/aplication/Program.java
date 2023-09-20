56865b907c6c958d72a7d1df88eabac0353533fa
jmoka <jmokatavares@gmail.com>
2023-09-20 07:29:30 -0300

    Criação class seller

--- demo-dao-jdbc/src/aplication/Program.java
+++ demo-dao-jdbc/src/aplication/Program.java
@@ -1,15 +1,21 @@
 package aplication;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		Department obj = new Department(1, "livro");
+		Department obj1 = new Department(2, "carros");
 		System.out.println(obj);
 		
+		
+		Seller seller = new Seller(1, "jota", "jota@gmail.com", new Date(), 300.00, obj1 );
+		System.out.println(seller);
 	}
 
 }
