fd560bb19c081774b93185e50e0f7408bbc3d8d2
teusiaqe <jmokatavares@gmail.com>
2023-10-07 19:46:11 -0300

    Implementação entite payment

--- curso_springBoot/curso_springBoot/src/main/java/com/jotaempresas/curso_springBoot/config/TesetConfig.java
+++ curso_springBoot/curso_springBoot/src/main/java/com/jotaempresas/curso_springBoot/config/TesetConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jotaempresas.curso_springBoot.entites.Category;
 import com.jotaempresas.curso_springBoot.entites.Order;
 import com.jotaempresas.curso_springBoot.entites.OrderItem;
+import com.jotaempresas.curso_springBoot.entites.Payment;
 import com.jotaempresas.curso_springBoot.entites.Product;
 import com.jotaempresas.curso_springBoot.entites.User;
 import com.jotaempresas.curso_springBoot.repositories.CategoryRepository;
@@ -86,6 +87,12 @@ public class TesetConfig implements  CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
+		
 		
 	}
 
--- curso_springBoot/curso_springBoot/src/main/java/com/jotaempresas/curso_springBoot/entites/Order.java
+++ curso_springBoot/curso_springBoot/src/main/java/com/jotaempresas/curso_springBoot/entites/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +42,11 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order", fetch = FetchType.EAGER)
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+		
+	private Payment payment;
+	
 
 
 	public Order() {
@@ -80,6 +87,14 @@ public class Order implements Serializable {
 	public Set<OrderItem>getItems(){
 		return items;
 	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	@Override
 	public int hashCode() {
@@ -100,7 +115,12 @@ public class Order implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
+		return "Order [id=" + id + ", moment=" + moment + ", client=" + client + ", items=" + items + ", payment="
+				+ payment + "]";
 	}
+
+
+
+
 
 }
