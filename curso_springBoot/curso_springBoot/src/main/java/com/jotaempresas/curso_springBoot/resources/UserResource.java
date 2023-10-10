46e6cdb9aca430abc63edc2f8cad5b98af78eb26
jmoka <jmokatavares@gmail.com>
2023-10-10 20:53:52 -0300

    Metodo Inser User -  iniserção de usuario

--- curso_springBoot/curso_springBoot/src/main/java/com/jotaempresas/curso_springBoot/resources/UserResource.java
+++ curso_springBoot/curso_springBoot/src/main/java/com/jotaempresas/curso_springBoot/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.jotaempresas.curso_springBoot.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jotaempresas.curso_springBoot.entites.User;
 import com.jotaempresas.curso_springBoot.servicies.UserService;
@@ -32,5 +36,22 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	/*
+	 * 
+	 * dessa forma retorna o codigo 200, mais o ideal e o retorno do código 201 para isso iremos usar de outra forma
+	
+	@PostMapping
+	public ResponseEntity<User> insert (@RequestBody User objUser ){
+		objUser = service.insert(objUser);
+		return ResponseEntity.created(loca)
+	}
+ */
+	
+	@PostMapping
+	public ResponseEntity<User> insert (@RequestBody User objUser ){
+		objUser = service.insert(objUser);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objUser.getId()).toUri();
+		return ResponseEntity.created(uri).body(objUser);
+}
 }
